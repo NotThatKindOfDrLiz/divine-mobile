88160ff4d572630914cb0527d4230710bce81581
John Weidner <john.weidner@verygood.ventures>
2026-01-29 20:48:51 -0600

    generated code

--- mobile/lib/providers/nostr_client_provider.g.dart
+++ mobile/lib/providers/nostr_client_provider.g.dart
@@ -50,7 +50,7 @@ final class NostrServiceProvider
   }
 }
 
-String _$nostrServiceHash() => r'3185bc6bcc6110fe7c18f563f9f6253de03d84e6';
+String _$nostrServiceHash() => r'511b3e3967ce08f7a311d8ad7b5538032bc0dbc0';
 
 /// Core Nostr service via NostrClient for relay communication
 /// Uses a Notifier to react to auth state changes and recreate the client
